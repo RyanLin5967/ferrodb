32eb3eaf09c522ab3abaeee05fc68d5a199d3834
RyanLin5967 <idident5967@gmail.com>
2026-05-28 23:58:48 -0400

    feat: ordering for Value

--- src/catalog/column.rs
+++ src/catalog/column.rs
@@ -12,7 +12,7 @@ pub enum DataType {
     Boolean,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(Debug)]
 pub enum Value {
     Integer(i32),
     Float(f64),
@@ -26,3 +26,33 @@ impl Column {
         Column {name, data_type, nullable}
     }
 }
+
+impl Ord for Value {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        use std::cmp::Ordering;
+        match (self, other) {
+            (Value::Integer(a), Value::Integer(b)) => a.cmp(b),
+            (Value::Float(a), Value::Float(b)) => a.total_cmp(b),
+            (Value::Boolean(a), Value::Boolean(b)) => a.cmp(b),
+            (Value::Varchar(a), Value::Varchar(b)) => a.cmp(b),
+            (Value::Null, Value::Null) => Ordering::Equal, 
+            (Value::Null, _) => Ordering::Less, // so nulls are first
+            (_, Value::Null) => Ordering::Greater,
+            _ => Ordering::Equal,
+        }
+    }
+}
+
+impl PartialOrd for Value {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Value {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other) == std::cmp::Ordering::Equal
+    }
+}
+
+impl Eq for Value {}
