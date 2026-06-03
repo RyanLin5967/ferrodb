55d521ae3667ce9809b90d4a70252a0565ff83d7
RyanLin5967 <idident5967@gmail.com>
2026-06-02 20:05:09 -0400

    feat: statement parsing

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,4 +1,4 @@
-use crate::{catalog::column::Column, error::FerroError, parser::scanner::{Scanner, Token, TokenType::{self}}};
+use crate::{catalog::column::{Column, DataType}, error::FerroError, parser::scanner::{Scanner, Token, TokenType::{self}}};
 
 pub struct Parser {
     pub tokens: Vec<Token>,
@@ -29,13 +29,13 @@ pub enum Expr {
 #[derive(Debug, Clone)]
 pub enum Stmt {
     Select {
-        columns: Vec<Expr>,
         table: String,
+        columns: Vec<Expr>,
         where_clause: Option<Expr>
     },
     Insert {
         table: String,
-        values: String
+        values: Vec<Expr>
     },
     Update {
         table: String,
@@ -47,12 +47,12 @@ pub enum Stmt {
         where_clause: Option<Expr>
     },
     CreateTable {
-        name: String,
+        table: String,
         columns: Vec<Column>,
     },
     CreateIndex {
         index_name: String,
-        table_name: String,
+        table: String,
         column_name: String
     }
 }
@@ -78,8 +78,172 @@ impl Parser {
         statements
     }
     pub fn parse_statement(&mut self) -> Result<Stmt, FerroError>{
-        todo!()
+        if self.match_token(&[TokenType::Select]) {
+            return self.parse_select()
+        } else if self.match_token(&[TokenType::Insert]) {
+            return self.parse_insert()
+        } else if self.match_token(&[TokenType::Update]) {
+            return self.parse_update()
+        } else if self.match_token(&[TokenType::Delete]) { 
+            return self.parse_delete()
+        } else if self.match_token(&[TokenType::Create]){
+            if self.match_token(&[TokenType::Index]) {
+                return self.parse_create_index()
+            } else if self.match_token(&[TokenType::Table]) {
+                return self.parse_create_table()
+            } else {
+                return Err(Parser::error(self.peek(), "expected TABLE or INDEX after CREATE".into()));
+            }
+        } else {
+            return Err(Parser::error(self.peek(), "expected a statement".into()));
+        }
     }
+
+    // SELECT vals FROM table WHERE expr (optional)
+    pub fn parse_select(&mut self) -> Result<Stmt, FerroError>{
+        let mut columns = Vec::new();
+        if self.match_token(&[TokenType::Star]) {
+            columns.push(Expr::ColumnRef("*".to_string()));
+        }else {
+            loop {
+                columns.push(self.expression()?);
+                if !self.match_token(&[TokenType::Comma]) {break;}
+            }
+        }
+        self.consume(TokenType::From, "expected FROM")?;
+        let table = self.consume(TokenType::Identifier, "expected table name")?.lexeme;
+        let where_clause = if self.match_token(&[TokenType::Where]) {
+            Some(self.expression()?)
+        } else {
+            None
+        };
+        self.consume(TokenType::Semicolon, "expected ;")?;
+        Ok(Stmt::Select { table, columns, where_clause})
+    }
+
+    // INSERT INTO table VALUES vals
+    pub fn parse_insert(&mut self) -> Result<Stmt, FerroError>{
+        if !self.match_token(&[TokenType::Into]) {
+            return Err(Parser::error(self.peek(), "expected INTO".into()));
+        } 
+        let table = self.consume(TokenType::Identifier, "expected table name")?.lexeme;
+        if !self.match_token(&[TokenType::Values]) {
+            return Err(Parser::error(self.peek(), "expected VALUES".into()));
+        }
+        self.consume(TokenType::LeftParen, "expected (")?;
+        let mut values = Vec::new();
+        loop {
+            values.push(self.expression()?);
+            if !self.match_token(&[TokenType::Comma]) {break;}
+        }
+        self.consume(TokenType::RightParen, "expected )")?;
+        self.consume(TokenType::Semicolon, "expected ;")?;
+        Ok(Stmt::Insert { table, values })
+    }
+
+    // UPDATE table SET col = val WHERE expr (optional)
+    pub fn parse_update(&mut self) -> Result<Stmt, FerroError>{
+        let table = self.consume(TokenType::Identifier, "expected table name")?.lexeme;
+        if !self.match_token(&[TokenType::Set]) {
+            return Err(Parser::error(self.peek(), "expected SET".into()));
+        }
+        let mut assignments: Vec<(String, Expr)> = Vec::new();
+
+        loop {
+            let column_name = self.consume(TokenType::Identifier, "expected column name")?.lexeme;
+            self.consume(TokenType::Equal, "expected =")?;
+            let value = self.expression()?;
+            assignments.push((column_name, value));
+            if !self.match_token(&[TokenType::Comma]) {break;}
+        }
+
+        let where_clause = if self.match_token(&[TokenType::Where]) {
+            Some(self.expression()?)
+        } else {
+            None
+        };
+        self.consume(TokenType::Semicolon, "expected ;")?;
+        Ok (Stmt::Update { table, assignments, where_clause })
+    }
+
+    // DELETE FROM table WHERE expr
+    pub fn parse_delete(&mut self) -> Result<Stmt, FerroError>{
+        if !self.match_token(&[TokenType::From]) {
+            return Err(Parser::error(self.peek(), "expected FROM".into()));
+        }
+        let table = self.consume(TokenType::Identifier, "expected table name")?.lexeme;
+        let where_clause = if self.match_token(&[TokenType::Where]) {
+            Some(self.expression()?)
+        } else {
+            None
+        };
+
+        self.consume(TokenType::Semicolon, "expected ;")?;
+        Ok(Stmt::Delete { table, where_clause })
+    }
+
+    // CREATE TABLE name (col, datatype, (null/not null)...)
+    pub fn parse_create_table(&mut self) -> Result<Stmt, FerroError> {
+        let table = self.consume(TokenType::Identifier, "expected table name")?.lexeme;
+        let mut columns = Vec::new();
+        self.consume(TokenType::LeftParen, "expected (")?;
+        loop {
+            let name = self.consume(TokenType::Identifier, "expected col name")?.lexeme;
+            let data_type = if self.match_token(&[TokenType::TypeInt]) {
+                DataType::Integer
+            } else if self.match_token(&[TokenType::TypeBoolean]) {
+                DataType::Boolean
+            } else if self.match_token(&[TokenType::TypeFloat]) {
+                DataType::Float
+            } else if self.match_token(&[TokenType::TypeVarchar]) {
+                self.consume(TokenType::LeftParen, "expected ( after VARCHAR")?;
+                let size_token = self.consume(TokenType::Number, "expected size")?;
+                let size: u16 = size_token.lexeme.parse().map_err(|_| Parser::error(size_token.clone(), "invalid size".into()))?;
+                self.consume(TokenType::RightParen, "expected )")?;
+                DataType::Varchar(size)
+            }else {
+                return Err(Parser::error(self.peek(), "expected data type".into()));
+            };
+
+            let mut nullable = true;
+            if self.match_token(&[TokenType::Not]) {
+                if self.match_token(&[TokenType::Null]) {
+                    nullable = false;
+                } else {
+                    return Err(Parser::error(self.peek(), "unexpected NOT".into()));
+                }
+            } else if self.match_token(&[TokenType::Null]) {
+                nullable = true;
+            }
+            columns.push(Column { name, data_type, nullable });
+
+            if !self.match_token(&[TokenType::Comma]) {
+                break;
+            }
+        }
+        self.consume(TokenType::RightParen, "expected )")?;
+        self.consume(TokenType::Semicolon, "expected ;")?;
+        Ok(Stmt::CreateTable { table, columns })
+    }
+
+    // CREATE INDEX index_name ON table (col)
+    pub fn parse_create_index(&mut self) -> Result<Stmt, FerroError> {
+        let index_name = self.consume(TokenType::Identifier, "expected index name")?.lexeme;
+        if !self.match_token(&[TokenType::On]) {
+            return Err(Parser::error(self.peek(), "expected ON".into()));
+        }
+
+        let table = self.consume(TokenType::Identifier, "expected table name")?.lexeme;
+        self.consume(TokenType::LeftParen, "expected (")?;
+        let column_name = self.consume(TokenType::Identifier, "expected column name")?.lexeme;
+        if self.check(TokenType::Comma) {
+            return Err(Parser::error(self.peek(), "composite indexes not supported yet".into()));
+        }
+        self.consume(TokenType::RightParen, "expected )")?;
+        self.consume(TokenType::Semicolon, "expected ;")?;
+        Ok(Stmt::CreateIndex { index_name, table, column_name })
+    }
+
     pub fn match_token(&mut self, types: &[TokenType]) -> bool{
         for token_type in types {
             if self.check(*token_type) {
