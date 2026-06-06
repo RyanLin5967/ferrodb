cf75c775eebfd6a14c2236f76a5143a731c33d2a
RyanLin5967 <idident5967@gmail.com>
2026-06-06 02:47:07 -0400

    feat: deserialize on filter -> deserialize on scan

--- src/execution/seq_scan.rs
+++ src/execution/seq_scan.rs
@@ -1,11 +1,22 @@
 use crate::{error::FerroError, execution::executor::Executor, storage::{heap_scanner::HeapScanner, tuple::Tuple}};
 use crate::storage::heap_file_manager::RecordId;
+use crate::catalog::column::Value;
+use crate::catalog::schema::Schema;
 pub struct SeqScan {
-    pub scanner: HeapScanner
+    pub scanner: HeapScanner,
+    pub schema: Schema,
 }
 
 impl Executor for SeqScan {
-    fn next(&mut self) -> Option<Result<(RecordId, Tuple), FerroError>> {
-        self.scanner.next()
+    fn next(&mut self) -> Option<Result<(RecordId, Vec<Value>), FerroError>> {
+        let (rid, tuple) = match self.scanner.next()? {
+            Ok((r, t)) => (r, t),
+            Err(e) => return Some(Err(e))
+        };
+        let values = match tuple.deserialize(&self.schema) {
+            Ok(v) => v,
+            Err(e) => return Some(Err(e)),
+        };
+        Some(Ok((rid, values)))
     }
 }
