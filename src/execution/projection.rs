d291a9bb700949dee36c49d5af4b6d9463392733
RyanLin5967 <idident5967@gmail.com>
2026-06-06 16:31:16 -0400

    feat: projection operator

---
+++ src/execution/projection.rs
@@ -0,0 +1,29 @@
+use crate::catalog::schema::Schema;
+use crate::execution::executor::{Executor, evaluate};
+use crate::error::FerroError;
+use crate::storage::heap_file_manager::RecordId;
+use crate::catalog::column::Value;
+use crate::parser::parser::Expr;
+
+pub struct Projection {
+    pub child: Box<dyn Executor>,
+    pub columns: Vec<Expr>,
+    pub schema: Schema
+}
+
+impl Executor for Projection {
+    fn next(&mut self) -> Option<Result<(RecordId, Vec<Value>), FerroError>> {
+        let (rid, values) = match self.child.next()? {
+            Ok((r, v)) => (r, v),
+            Err(e) => return Some(Err(e))
+        };
+        let mut eval_values = Vec::new();
+        for expr in &self.columns {
+            eval_values.push( match evaluate(expr, &values, &self.schema) {
+                Ok(v) => v,
+                Err(e) => return Some(Err(e))
+            })
+        }
+        Some(Ok((rid, eval_values)))
+    }
+}
