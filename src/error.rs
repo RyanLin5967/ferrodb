81231d47cd7dfb9fc5946f97a880231a813709c5
RyanLin5967 <idident5967@gmail.com>
2026-06-06 18:51:12 -0400

    feat: constraint error type

--- src/error.rs
+++ src/error.rs
@@ -12,6 +12,7 @@ pub enum FerroError {
     PagePinned,
     SqlParseError(String),
     IndexAlreadyExists,
+    Contraint(String)
 }
 
 impl Display for FerroError {
@@ -26,6 +27,7 @@ impl Display for FerroError {
             FerroError::PagePinned => write!(f, "page is pinned"),
             FerroError::SqlParseError(s) => write!(f, "sql parsing error: {}", s),
             FerroError::IndexAlreadyExists => write!(f, "index already exists"),
+            FerroError::Contraint(s) => write!(f, "contraint error: {}", s)
         }
     }
 }
